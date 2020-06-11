e3e074168c4a0bc466aef9a1d5a9d5c80c5a9876
mloftin <micah.loftin@wellsky.com>
2020-06-11 12:54:46 -0500

    Pushing fix for tier 2/3 explicit visibility in call pivot data.

--- QlikSense-SQLScripts/call_answers/qlik_call_answer_pivot_view.sql
+++ QlikSense-SQLScripts/call_answers/qlik_call_answer_pivot_view.sql
@@ -12,7 +12,18 @@ DECLARE
     _final_query TEXT;
     _call_limit VARCHAR;
 BEGIN
-    -- Version 20200605-1
+    -- Version 20200611-1
+
+    CREATE TEMP TABLE tmp_qlik_vis_provider AS
+    SELECT visibility_id, vgpt.provider_id
+    FROM qlik_answer_vis_array qav 
+    JOIN sp_visibility_group_provider_tree vgpt ON (vgpt.visibility_group_id = ANY(allow_ids))
+    JOIN (SELECT DISTINCT provider_id FROM qlik_user_access_tier_view WHERE user_access_tier != 1) up ON (vgpt.provider_id = up.provider_id)
+    EXCEPT
+    SELECT visibility_id, vgpt.provider_id
+    FROM qlik_answer_vis_array qav 
+    JOIN sp_visibility_group_provider_tree vgpt ON (vgpt.visibility_group_id = ANY(deny_ids))
+    JOIN (SELECT DISTINCT provider_id FROM qlik_user_access_tier_view WHERE user_access_tier != 1) up ON (vgpt.provider_id = up.provider_id);
 
     _types := CASE WHEN ($3 IS NULL) THEN ARRAY['call', 'call_followup'] ELSE $3 END;
 
@@ -24,21 +35,27 @@ BEGIN
 
         _inner_query := 'SELECT DISTINCT ON (tier_link, call_record_id, virt_field_name) tier_link||''''|''''||call_record_id AS sec_key, virt_field_name, answer_val
                  FROM (
+                 -- Tier 1 - Top answers
                  SELECT DISTINCT cr.call_record_id, tier_link, virt_field_name, answer_val, date_effective 
                  FROM qlik_'||_type||'_answers qea
                  JOIN sp_call_record cr USING (call_record_id)
                  JOIN (SELECT DISTINCT tier_link, provider_id AS provider_creating_id FROM qlik_user_access_tier_view t WHERE t.user_access_tier = 1) uat USING (provider_creating_id)
                  WHERE end_date IS NULL OR end_date::DATE >= '''''||$2||'''''::DATE
                  UNION
+                 -- Tier 2/3 - INHERITED
                  SELECT DISTINCT cr.call_record_id, uat.tier_link, virt_field_name, answer_val, date_effective
                  FROM qlik_call_answer_access qaa
-                 JOIN sp_call_record cr ON (qaa.call_record_id = cr.call_record_id AND '||_call_limit||')
+                 JOIN sp_call_record cr ON (qaa.provider_id = cr.provider_creating_id AND qaa.call_record_id = cr.call_record_id AND '||_call_limit||')
                  JOIN (SELECT DISTINCT tier_link, provider_id AS provider_creating_id FROM qlik_user_access_tier_view t WHERE t.user_access_tier = 1) uat USING (provider_creating_id)
                  WHERE end_date IS NULL OR end_date::DATE >= '''''||$2||'''''::DATE 
-                   AND (cr.provider_creating_id = qaa.provider_id
-                   OR (qaa.visibility_id IS NOT NULL 
-                       AND EXISTS (SELECT 1 FROM qlik_answer_vis_provider qap WHERE qap.visibility_id = qaa.visibility_id AND qap.provider_id = qaa.provider_id)))
-                 ) t
+                 UNION
+                 -- Tier 2/3 - EXPLICIT
+                 SELECT DISTINCT cr.call_record_id, uat.user_access_tier||''''|''''||qavp.provider_id AS tier_link, virt_field_name, answer_val, date_effective
+                 FROM qlik_call_answer_access qaa 
+                 JOIN sp_call_record cr ON (qaa.provider_id = cr.provider_creating_id AND qaa.call_record_id = cr.call_record_id AND '||_call_limit||')
+                 JOIN tmp_qlik_vis_provider qavp USING (visibility_id)
+                 CROSS JOIN (SELECT DISTINCT user_access_tier FROM qlik_user_access_tier_view WHERE user_access_tier != 1) uat
+                 WHERE end_date IS NULL OR end_date::DATE >= '''''||$2||'''''::DATE) t
                  ORDER BY call_record_id, tier_link, virt_field_name, date_effective DESC';
 
         _dsql := 'SELECT FORMAT(
